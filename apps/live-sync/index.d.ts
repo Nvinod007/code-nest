d828fb36294d270c30f1f111427c8f55dea80d26
Vinod Kumar N <vinodkumar.nelanakula@carrier.com>
2025-03-25 00:03:15 +0530

    remoce e2e for now

--- apps/live-sync/index.d.ts
+++ apps/live-sync/index.d.ts
@@ -1,5 +1,5 @@
-declare module '*.svg' {
-  import type { FC, SVGProps } from 'react';
+declare module "*.svg" {
+  import type { FC, SVGProps } from "react";
 
   export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
   const content: string;
--- apps/live-sync/src/app/api/hello/route.ts
+++ apps/live-sync/src/app/api/hello/route.ts
@@ -1,3 +1,3 @@
 export async function GET(request: Request): Promise<Response> {
-  return new Response('Hello, from API!');
+  return new Response("Hello, from API!");
 }
