d70bbfd179408dedf9084503be950cc7016773c5
Vinod Kumar N <vinodkumar.nelanakula@carrier.com>
2025-06-04 08:34:49 +0530

    build fix

--- apps/live-sync/next.config.js
+++ apps/live-sync/next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  webpack: (config, { isServer }) => {
+    config.cache = false;
+    return config;
+  },
 };
 
 const plugins = [
--- libs/themes/src/lib/themes.ts
+++ libs/themes/src/lib/themes.ts
@@ -1,3 +1,47 @@
-export function themes(): string {
-  return "themes";
-}
+const plugin = require("tailwindcss/plugin");
+import { CSSRuleObject } from "tailwindcss/types/config";
+
+export const tailwindPlugin = plugin(
+  function ({
+    addBase,
+    addComponents,
+    addUtilities,
+    theme,
+  }: {
+    addBase: (base: CSSRuleObject | CSSRuleObject[]) => void;
+    addComponents: (components: CSSRuleObject | CSSRuleObject[]) => void;
+    addUtilities: (utilities: CSSRuleObject | CSSRuleObject[]) => void;
+    theme: (path?: string) => unknown;
+  }) {
+    addBase({
+      h1: {
+        fontSize: theme("fontSize.2xl") as string,
+      },
+    });
+    addComponents({
+      ".card": {
+        backgroundColor: theme("colors.white") as string,
+        borderRadius: theme("borderRadius.lg") as string,
+        padding: theme("padding.6") as string,
+      },
+    });
+    addUtilities({
+      ".rotate-180": {
+        transform: "rotate(180deg)",
+      },
+    });
+  },
+  {
+    theme: {
+      extend: {
+        colors: {
+          brand: {
+            DEFAULT: "#2292BD",
+            dark: "#005073",
+            light: "#3fbaeb",
+          },
+        },
+      },
+    },
+  }
+);
