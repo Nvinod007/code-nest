1a31aa139fc759869633345e86b19b9571418172
Nvinod007 <vinodkumar.nelanakula@gmail.com>
2026-06-14 13:35:00 +0530

    lint fix

--- apps/live-sync/jest.config.ts
+++ apps/live-sync/jest.config.ts
@@ -1,12 +1,12 @@
 const config = {
-  displayName: 'live-sync',
-  preset: '../../jest.preset.js',
+  displayName: "live-sync",
+  preset: "../../jest.preset.js",
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
+    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nx/next/babel"] }],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: 'test-output/jest/coverage',
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "test-output/jest/coverage",
 };
 
 export default config;
